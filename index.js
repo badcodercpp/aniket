b79b2a8cf6ff005e44ca63b318cc934ad87603a4
ajay <ajaykrjha93@gmail.com>
2018-08-06 18:28:15 +0530

    demo

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ io.on('connection', function(socket) {
 	}
 	//socket.emit('',connected[room[m.to]])
 	let too=room[m.to];
-   socket.emit('chatting',JSON.stringify)
+   io.to(too).emit('chatting',JSON.stringify(m))
    })
 });
 
