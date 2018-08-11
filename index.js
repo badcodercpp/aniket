06a217dee25a3c3353ae82df7bed6380a44e6b72
ajay <ajaykrjha93@gmail.com>
2018-08-11 18:19:07 +0530

    demo

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ io.on('connection', function(socket) {
   });
   socket.on('join', function(name, callback){
     console.log('join', name);
-    //var socketIds = socketIdsInRoom(name);
+    var socketIds = socketIdsInRoom(name);
 	//let ar=[];
 	//ar.push(socket.id)
 //var socketIds=ar
