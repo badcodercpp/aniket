34b3a7e8eb41c6bc3a1930ed289b48a9fee67186
ajay <ajaykrjha93@gmail.com>
2018-08-11 14:37:51 +0530

    demo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express=require('express')
 var path=require('path')
 var app = express();
 var http = require('http').createServer(app);
-const io = require('socket.io')(server, {pingTimeout: 30000})
+const io = require('socket.io')(http, {pingTimeout: 30000})
 const PORT = process.env.PORT || 5000
 
 //var room={}
